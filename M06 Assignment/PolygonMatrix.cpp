2a82c39ce5e1e377838a7d9682318b71efe5f1c5
ArtistMonkey83 <yolie@yolandareyes.com>
2020-03-25 17:07:54 -0700

    Sans extra credit
    Passing all ten tests, no extra credit implemented

---
+++ M06 Assignment/PolygonMatrix.cpp
@@ -0,0 +1,116 @@
+#include "PolygonMatrix.h"
+
+namespace Graph_lib
+{
+///used to map from pixels to Cartesian coordinates and back
+//int mapRange(int , int fromLow, int fromHigh, int toLow, int toHigh)
+//{return toLow+((double)n-fromLow)*(toHigh-toLow)/(fromHigh-fromLow);}
+double mapRange(double n, double fromLow, double fromHigh, double toLow, double toHigh)
+{
+  return toLow+((double)n-fromLow)*(toHigh-toLow)/(fromHigh-fromLow);
+}
+///construct pointMatrix with 2 rows and 0 cols
+///store xPixels and yPixels for range mapping
+PolygonMatrix::PolygonMatrix(int _xPixels, int _yPixels): pointMatrix(2,0) //Fl::w() Fl::h()??
+{
+  xPixels = _xPixels;
+  yPixels = _yPixels;
+
+}
+
+///add a pixel vertex to the polygon
+///call the parent add function with Point p
+///use mapRange to map p.x and p.y to Cartesian coordinates
+///use addCoordinate to add the mapped x and y values to pointMatrix
+void PolygonMatrix::add(Point p) // passed argument is a pixel vertex from main mapped to Cartesian using mapRange
+{
+  //([0:xPixels],[yPixels :0])  <==> ([-xPixels / 2 : xPixels/2], [-yPixels/ 2 : yPixels/2])
+  //  from PolygonMatrix.h
+  //double mapRange(double n, double fromLow, double fromHigh, double toLow, double toHigh);
+
+  double pointX = mapRange(p.x,0,xPixels,(-xPixels/2), (xPixels/2));
+  double pointY = mapRange(p.y,yPixels,0,(-yPixels/2),(yPixels/2)); //dont flip the xs
+  //
+
+  pointMatrix.addCoordinate(pointX,pointY); //what value represents the x and y?
+                                            // p.y and p.x ???
+
+  Polygon::add(p);
+
+}
+
+///rotate polygon by theta radians counter-clockwise
+///construct a RotationMatrix R, mulitply it by pointMatrix
+///then call updatePixels
+void PolygonMatrix::rotate(double theta)
+{
+  RotationMatrix r(theta);
+  pointMatrix = r * pointMatrix;
+
+  updatePixels();
+}
+
+///Scale the size of the polygon by factor c
+///construct a ScalingMatrix S, multiply it by pointMatrix
+///then call updatePixels
+void PolygonMatrix::scale(double c)
+{
+  //ScalingMatrix::ScalingMatrix S() : Matrix (2,2){c,0,0,c}; in the class
+  ScalingMatrix s(c);
+  pointMatrix = s * pointMatrix;
+
+  updatePixels();
+}
+
+///shift the polygon by (xShift, yShift) coordinates
+///construct a TranslationMatrix T, add it to pointMatrix
+///then call updatePixels
+void PolygonMatrix::translate(double xShift, double yShift)
+{
+  //TranslationMatrix::TranslationMatrix T() : Matrix(2,n) {} //n is total number of stored points
+  TranslationMatrix t(xShift,yShift,pointMatrix.getCols()); //(xshift, y shift,pointMatrix.getCols());
+  pointMatrix = t + pointMatrix;
+
+  updatePixels();
+}
+
+///Const getter for the unit tester to view the internal matrix
+//const Matrix& getPointMatrix() const{return pointMatrix;}
+
+///Loop through each column of point matrix
+///map each coordinate to pixels
+///call Shape::set_point to update each pixel
+void PolygonMatrix::updatePixels()
+{
+  Point temp;
+  //int x,
+         //y;
+
+  for(int i = 0; i < pointMatrix.getCols(); i++)//loop through each column of pointMatrix
+  {
+    //double mapRange(double n, double fromLow, double fromHigh, double toLow, double toHigh);
+    //map cartesian coordinates to a Point containing x and y pixel values
+    //going from cartesian to pixel values
+    //x = pointMatrix[0][i];
+    //y = pointMatrix[1][i];
+
+    temp.x = mapRange(pointMatrix(0,i),(-xPixels/2),(xPixels/2),0,xPixels);
+    temp.y = mapRange(pointMatrix(1,i),(-yPixels/2),(yPixels/2),yPixels,0);
+
+
+    Polygon::set_point(i,temp); //updating point in the graphics lib for later drawing ????Shape::set_point(temp)????
+  }
+
+}
+
+
+
+
+/*
+Star::Star (inw, int h) : PolygonMatrix(w,h){}
+int centerX = w /2;
+int centerY = h/2;
+addpoint()
+set_fill_color()
+*/
+}
