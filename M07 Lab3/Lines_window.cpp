6e57bd6d00be6931cb724d6d44da7584dde25427
ArtistMonkey83 <yolie@yolandareyes.com>
2020-04-07 13:05:56 -0700

    Backing up VM
    Hasn't been tested

---
+++ M07 Lab3/Lines_window.cpp
@@ -0,0 +1,81 @@
+#include "Lines_window.h"
+
+Lines_window::Lines_window(Point xy,int w, int h, const string& title) : Window (xy,w,h,title),
+              next_button(Point(x_max()-150,0),70,20,"Next point", cb_next),
+              quit_button()(Point(x_max()-70,0),70,20,"Quit", cb_quit),
+              next_x(Point(x_max()-310,0),50,20,"Next X: "),
+              next_y(Point(x_max()-210,0),50,20,"Next Y: "),
+              xy_out(Point(100,0),100,20,"Current(x,y): "),
+              color_menu{Point{x_max()-70,30},60,20, Menu::vertical, "color"},
+              menu_button{Point{x_max()-80,30},80,20, "color menu", cb_menu},
+              style_menu{Point{x_max-100,30},70,20, Menu::vertical,"style"},
+              style_button{Point{x_max()-100,30},80,20,"style menu", cb_style}
+{
+  attach(next_button);
+  attach(quit_button);
+  attach(next_x);
+  attach(next_y);
+  attach(xy_out);
+  xy_out.put("no point");
+  color_menu.attach(new Button{Point{0,0}, 0,0 "red",cb_red});
+  color_menu.attach(new Button{Point{0,0}, 0,0 "blue",cb_blue});
+  color_menu.attach(new Button{Point{0,0}, 0,0 "black",cb_black});
+  attach(color_menu);
+  attach(style_menu);
+  color_menu.hide();
+  style_menu.hide();
+  attach(menu_button);
+  attach(style_button);
+  attach(lines);
+}
+
+void Lines_window::cb_menu(Address,Address pw)
+{reference_to<Lines_window>(pw).menu_pressed();}
+
+
+
+
+////////finish!!
+void Lines_window::cb_red(Address,Address pw)
+{reference_to<Lines_window>(pw).red_pressed();}
+
+void Lines_window::cb_blue(Address,Address pw)
+{reference_to<Lines_window>(pw).blue_pressed();}
+
+void Lines_window::cb_black(Address,Address pw)
+{reference_to<Lines_window>(pw).black_pressed();}
+
+void Lines_window::quit()
+{hide();}
+
+void Lines_window::next()
+{
+  int x = next_x.get_int();
+  int y = next_y.get_int();
+
+  lines.add(Point(x,y));
+
+  stringstream ss;
+  ss << '(' << x << ',' << y << ')';
+  xy_out.put(ss.str());
+
+  redraw();
+
+}
+void Lines_window::cb_quit(Address,Address pw)
+{ reference_to<Lines_window>(pw).quit();}
+
+void Lines_window:: cb_style(Addres,Address pw)
+{reference_to<Lines_window>(pw).style_pressed();}
+
+void Lines_window::cb_solid(Address,Address pw)
+{reference_to<Lines_window>(pw).solid_pressed();}
+
+void Lines_window::cb_dash(Address,Address pw)
+{reference_to<Lines_window>(pw).dash_pressed();}
+
+void Lines_window::cb_dot(Address,Address pw)
+{reference_to<Lines_window>(pw).dot_pressed();}
+
+void Lines_window::cb_next(Address,Address pw)
+{reference_to<Lines_window>(pw).next();}
---
+++ M07 Lab3/Lines_window.h
@@ -0,0 +1,55 @@
+#ifndef LINES_WINDOW_H
+#define LINES_WINDOW_H
+
+#include "../std_lib_facilities.h"
+#include "../GUI/GUI.h"
+
+using namespace  Graph_lib;
+
+struct Lines_window : Graph_lib::Window
+{
+  Lines_window(Point xy, int w, int h, const string& title);
+
+  private:
+    Open_polyline lines;
+
+    Button next_button,
+           quit_button,
+           style_button,
+           menu_button;
+    In_box next_x,
+           next_y;
+    Out_box xy_out;
+
+    Menu color_menu,
+         style_menu;
+
+    void change(Color c) {lines.set_color(c);}
+    void changeStyle(Line_style c) {lines.set_style(c);} ////////////////style needs changing!!!
+    void hide_menu() {color_menu.hide(); menu_button.show();}
+    void hide_style() {style_menu.hide(); styler_button.show();}
+
+    void red_pressed() {change(Color::red); hide_menu();}
+    void blue_pressed() {change(Color::blue); hide_menu();}
+    void black_pressed() {change(Color::black); hide_menu();}
+
+    void menu_pressed() {menu_button.hide(); color_menu.show();}
+    void style_pressed() {style_button.hide(); style_menu.show();}
+
+    void solid_pressed() {changeStyle(Line_style::solid); hide_style();}
+    void dash_pressed() {changeStyle(Line_style::dash).hide_style();}
+    void dot_pressed() {changeStyle(Line_style::dot); hide_style();}
+
+    void next();
+    void quit();
+
+    static void cb_red(Address, Address pw);
+    static void cb_blue(Address, Address pw);
+    static void cb_black(Address, Address pw);
+    static void cb_menu(Address, Address pw);
+    static void cb_style(Address, Address pw);
+    static void cb_quit(Address, Address pw);
+    static void cb_next(Address, Address pw);
+
+};
+#endif
